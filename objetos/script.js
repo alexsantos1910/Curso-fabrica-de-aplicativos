4392cfdbb446546e6d4f3b01829279c707be0c9f
alexsantos1910 <110111749+alexsantos1910@users.noreply.github.com>
2023-04-16 20:35:17 -0300

    aulas

---
+++ objetos/script.js
@@ -0,0 +1,12 @@
+
+
+//obejtos.
+
+let  pessoas = {
+    nome: 'Alex',
+    idade: 23,
+    altura: 1.78,
+    peso: 96
+}
+
+console.log(pessoas)
---
+++ temporizadores/script.js
@@ -0,0 +1,17 @@
+
+// setInterval
+
+
+function acao (){
+    document.write('Escrevendo ....')
+}
+/*
+setInterval(() => {
+    document.write('escrevendo ....<br>')
+}, 1000);
+
+*/
+
+setTimeout(() => {
+    alert('boa sorte!')
+}, 4000);
