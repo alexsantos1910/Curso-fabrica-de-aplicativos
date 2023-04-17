67e1e70de363dabd2e830cea755793512d114adc
alexsantos1910 <110111749+alexsantos1910@users.noreply.github.com>
2023-04-17 19:54:36 -0300

    concluido

--- lista-tarefas/script.js
+++ lista-tarefas/script.js
@@ -1,18 +1,64 @@
 
-let listElement = document.querySelector("#app ul")
-let inputElement = document.querySelector("#app input")
-let buttonElement = document.querySelector("#app button")
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
 
-let tarefas = []
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [] ;
 
-function adicionarTarefas (){
-    if(inputElement.value === ''){
-        alert('Digite alguma tarefa')
-        return false
-    }else {
-        let novaTarefa = inputElement.value
-        tarefas.push = (novaTarefa)
-        inputElement.value = ''
-    }
+function renderTarefas(){
+  listElement.innerHTML = "";
+
+  tarefas.map((todo)=>{
+    let liElement = document.createElement("li");
+    let tarefaText = document.createTextNode(todo);
+
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href","#");
+
+    let linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
+
+    let posicao = tarefas.indexOf(todo);
+
+    linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`)
+
+    liElement.appendChild(tarefaText);
+    listElement.appendChild(liElement);
+    liElement.appendChild(linkElement);
+
+
+  })
+
 }
-buttonElement.onclick = adicionarTarefas 
+
+renderTarefas();
+
+
+function adicionarTarefas(){
+  if(inputElement.value === ''){
+    alert("Digite alguma tarefa");
+    return false;
+  }else{
+    let novaTarefa = inputElement.value;
+
+    tarefas.push(novaTarefa);
+    inputElement.value = '';
+
+    renderTarefas();
+    salvaDados();
+
+  }
+
+}
+
+buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1);
+    renderTarefas();
+    salvaDados();
+}
+
+function salvaDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas))
+}
