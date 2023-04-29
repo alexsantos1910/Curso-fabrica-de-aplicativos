58642f07be8b3b11dfbf820b2c553341ded4e545
alexsantos1910 <110111749+alexsantos1910@users.noreply.github.com>
2023-04-29 20:44:12 -0300

    api extras

--- requisições/script.js
+++ requisições/script.js
@@ -5,37 +5,35 @@ let listElement = document.querySelector("#app");
 let posts = [];
 
 function nutriApp(){
-
     fetch("https://sujeitoprogramador.com/rn-api/?api=posts")
-    .then((r) => r.json())
-    .then((json) => {
+    .then((response) => response.json())
+    .then((json)=> {
         posts = json;
 
         posts.map((item) => {
-            let liElement = document.createElement("li");
-            let titleElement = document.createElement("strong");
-            let imgElement = document.createElement("img");
-            let descriptionElement = document.createElement("a");
+          let liElement = document.createElement("li");
+          let titleElement = document.createElement("strong");
+          let imgElement = document.createElement("img");
+          let decriptionElement = document.createElement("a");
 
-            let titleText = document.createTextNode(item.titulo);
-            titleElement.appendChild(titleText);
-            liElement.appendChild(titleElement);
+          let titleText = document.createTextNode(item.titulo);
+          titleElement.appendChild(titleText);
+          liElement.appendChild(titleElement);
 
-            imgElement.src = item.capa;
-            liElement.appendChild(imgElement);
+          imgElement.src = item.capa;
+          liElement.appendChild(imgElement);
 
-            let descriptionText = document.createTextNode(item.subtitulo);
-            descriptionElement.appendChild(descriptionText);
-            liElement.appendChild(descriptionElement);
+          let decriptionText = document.createTextNode(item.subtitulo);
+          decriptionElement.appendChild(decriptionText);
+          liElement.appendChild(decriptionElement);
 
-            listElement.appendChild(liElement);
+          listElement.appendChild(liElement);
         })
-
-    })
-    .catch(()=> {
-        console.log("Deu algo de errado")
     })
 
+    .catch(() => {
+        console.log("Deu algo de errado");
+    })
 }
 
-nutriApp()
+nutriApp();
